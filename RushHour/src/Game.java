c388f10904d6d8bcc280d79e2ae54d94126558ce
Gauthier <avite.gauthier@gmail.com>
2022-12-18 17:52:39 +0100

    factorisation

--- RushHour/src/Game.java
+++ RushHour/src/Game.java
@@ -2,23 +2,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-class Car {
-	int carNumber; // potentially useless
-	String orientation;
-	int length;
-	int x, y;
-
-	public Car(String line) {
-		String[] elements = line.split(" ");
-
-		this.carNumber = Integer.parseInt(elements[0]);
-		this.orientation = elements[1];
-		this.length = Integer.parseInt(elements[2]);
-		this.x = Integer.parseInt(elements[3]);
-		this.y = Integer.parseInt(elements[4]);
-	}
-
-}
 
 public class Game {
 	int size;
