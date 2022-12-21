e914ae0e8635e302a1a04273fb83858b10225634
Octave <octave.charrin@gmail.com>
2022-12-21 17:28:23 +0100

    modified Test.java to display the distance found

--- RushHour/src/Test.java
+++ RushHour/src/Test.java
@@ -9,32 +9,54 @@ public class Test {
 		long[][] res = new long[6][4];
 		for (int i = 0; i < 6; i++) {
 			Game game = new Game("RushHour" + (i + 1) + ".txt");
+			
+			System.out.println("\nfile : " +(i+1));
 
 			// BFS
 			long start = System.currentTimeMillis();
-			for (int iter = 0; iter < 10; iter++)
-				System.out.println("BFS : trial :" + iter + "; d= " + BreadthFirst.bfs(game));
+			for (int iter = 0; iter < 10; iter++) {
+				if (iter == 0) {
+					System.out.println("BFS : d= " + BreadthFirst.bfs(game));
+				} else {
+					BreadthFirst.bfs(game);
+				}
+			}
 			long end = System.currentTimeMillis();
 			res[i][0] = (end - start) / 10;
 
 			// First Heuristic
 			start = System.currentTimeMillis();
-			for (int iter = 0; iter < 10; iter++)
-				System.out.println("h1 : trial :" + iter + "; d= " + HeuristicSolver.search(game, 1));
+			for (int iter = 0; iter < 10; iter++) {
+				if (iter == 0) {
+					System.out.println("h1 : d= " + HeuristicSolver.search(game, 1));
+				} else {
+					HeuristicSolver.search(game, 1);
+				}
+			}
 			end = System.currentTimeMillis();
 			res[i][1] = (end - start) / 10;
 
 			// Second Heuristic
 			start = System.currentTimeMillis();
-			for (int iter = 0; iter < 10; iter++)
-				System.out.println("h2 : trial :" + iter + "; d= " + HeuristicSolver.search(game, 2));
+			for (int iter = 0; iter < 10; iter++) {
+				if (iter == 0) {
+					System.out.println("h2 : d= " + HeuristicSolver.search(game, 2));
+				} else {
+					HeuristicSolver.search(game, 2);
+				}
+			}
 			end = System.currentTimeMillis();
 			res[i][2] = (end - start) / 10;
 
 			// Third Heuristic
 			start = System.currentTimeMillis();
-			for (int iter = 0; iter < 10; iter++)
-				System.out.println("h3 : trial :" + iter + "; d= " + HeuristicSolver.search(game, 3));
+			for (int iter = 0; iter < 10; iter++) {
+				if (iter == 0) {
+					System.out.println("h3 : d= " + HeuristicSolver.search(game, 3));
+				} else {
+					HeuristicSolver.search(game, 3);
+				}
+			}
 			end = System.currentTimeMillis();
 			res[i][3] = (end - start) / 10;
 
@@ -46,7 +68,7 @@ public class Test {
 
 		for (int i = 0; i < res.length; i++) {
 			System.out.print("RushHour" + (i + 1) + ".txt");
-			
+
 			for (long elem : res[i]) {
 				System.out.print(" | ");
 				System.out.print(String.format("%3d%n", elem).replace(System.lineSeparator(), ""));
